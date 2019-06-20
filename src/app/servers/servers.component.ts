c12fba2d93a529f9d780465b666c2fbec8cea9d7
OleksiiSliusarenko <oleksii.sliusarenko@thomascookonline.com>
2019-06-20 19:17:44 +0300

    4 ways of databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,29 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  //templateUrl: './servers.component.html',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer: boolean = false;
+  serverName: string = 'default name';
+
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log((<HTMLInputElement>event.target).value);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
