2c9386f83623f85e890cc490042bd23c6229b280
OleksiiSliusarenko <oleksii.sliusarenko@thomascookonline.com>
2019-06-26 16:40:24 +0300

    Added ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,29 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'siple descr',
-      'https://www.maxpixel.net/static/photo/1x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg'),
-    new Recipe('A test recipe 2', 'siple descr',
-      'https://www.maxpixel.net/static/photo/1x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg')
+    new Recipe(
+      'A test recipe',
+      'siple descr',
+      'https://www.maxpixel.net/static/photo/1x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('French Fry', 10)
+      ]),
+    new Recipe(
+      'A test recipe 2',
+      'siple descr',
+      'https://www.maxpixel.net/static/photo/1x/Food-Vegetable-White-Kidney-Bean-Recipe-2728708.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Tomato', 1)
+
+      ])
   ];
 
   getRecipes() {
