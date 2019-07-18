efec4b0b99610cf6c6cae93a3c19e07f9c1b433e
OleksiiSliusarenko <oleksii.sliusarenko@thomascookonline.com>
2019-07-18 11:06:33 +0300

    Basic implementation of the Auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,55 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthResponse, AuthService} from './auth.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+})
+export class AuthComponent {
+
+  constructor(private authService: AuthService) {
+  }
+
+  isLoginMode: boolean = true;
+  isLoading: boolean = false;
+  error: string = null;
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(authForm: NgForm) {
+    if (authForm.invalid) {
+      return;
+    }
+    this.isLoading = true;
+
+    const email = authForm.value.email;
+    const password = authForm.value.password;
+
+    const authObs = this.signupOrSignin(email, password);
+
+    authObs.subscribe(response => {
+      console.log(response);
+      this.isLoading = false;
+    }, errorMessage => {
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
+
+    authForm.reset();
+  }
+
+  private signupOrSignin(email, password) {
+    let authObs: Observable<AuthResponse>;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+    return authObs;
+  }
+}
